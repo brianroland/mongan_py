d4300fb7e1e049c9db85751216e230e1361b46b0
Christopher Roland <brianroland@Christophers-Mac-mini.local>
2016-10-19 10:24:05 -0400

    BR: finished problem 54

--- mongan_54/lowest_common_ancestor.py
+++ mongan_54/lowest_common_ancestor.py
@@ -1,11 +1,50 @@
 
 
 
-
 class Node(object):
 
     def __init__(self, value):
+        self.parent = None
         self.left = None
         self.right = None
         self.value = value
 
+
+def on_same_side(smaller, larger, target):
+    return (smaller.value <= target.value) == (larger.value <= target.value)
+
+
+def split_by(smaller, larger, target):
+    return (smaller.value <= target.value) and (larger.value > target.value)
+
+
+def lowest_common_ancestor(node_x, node_y):
+    print 'LowestCommonAncestor: beg'
+
+    assert node_x is not None
+    assert node_y is not None
+
+    smaller = node_x
+    larger = node_y
+    if node_y.value < node_x.value:
+        smaller = node_y
+        larger = node_x
+
+    current = smaller.parent                    #know that smaller has a parent
+    parent = smaller.parent.parent              #this could be null
+
+    while True:
+
+        if parent is None:
+            return current
+
+        elif split_by(smaller, larger, current) and on_same_side(smaller, larger, parent):
+            return current
+
+        else:
+            current = parent
+            parent = current.parent
+
+
+
+    print 'LowestCommonAncestor: end'
--- mongan_54/tests_54/test_lowest_common_ancestor.py
+++ mongan_54/tests_54/test_lowest_common_ancestor.py
@@ -10,3 +10,90 @@ class TestLowestCommonAncestor(unittest.TestCase):
     def test_node(self):
         node = Node(0)
         print "node.value={0:d}".format(node.value)
+
+    def test_three_node_tree(self):
+        print 'test_lowest_common_ancestor: beg'
+
+        a = Node(0)
+        b = Node(1)
+        c = Node(2)
+
+        a.left = b
+        a.right = c
+        b.parent = a
+        c.parent = a
+
+        out = lowest_common_ancestor(b, c)
+
+        self.assertEqual(out.value,a.value)
+        print "ancestor has value={0:d}".format(out.value)
+
+        print 'test_lowest_common_ancestor: end'
+
+    def test_seven_node_tree(self):
+        print 'test_seven_node_tree: beg'
+
+        a = Node(80)
+        b = Node(40)
+        c = Node(120)
+        d = Node(20)
+        e = Node(60)
+        h = Node(10)
+        i = Node(30)
+
+        a.left = b
+        a.right = c
+        b.parent = a
+        c.parent = a
+
+        b.left = d
+        d.parent = b
+
+        b.right = e
+        e.parent = b
+
+        h.parent = d
+        d.left = h
+        i.parent = d
+        d.right = i
+
+        out = lowest_common_ancestor(h, e)
+
+        print "ancestor has value={0:d}".format(out.value)
+        self.assertEqual(out.value, b.value)
+
+        print 'test_seven_node_tree: end'
+
+    def test_seven_node_tree_again(self):
+        print 'test_seven_node_tree_again: beg'
+
+        a = Node(80)
+        b = Node(40)
+        c = Node(120)
+        d = Node(20)
+        e = Node(60)
+        h = Node(10)
+        i = Node(30)
+
+        a.left = b
+        a.right = c
+        b.parent = a
+        c.parent = a
+
+        b.left = d
+        d.parent = b
+
+        b.right = e
+        e.parent = b
+
+        h.parent = d
+        d.left = h
+        i.parent = d
+        d.right = i
+
+        out = lowest_common_ancestor(h, c)
+
+        print "ancestor has value={0:d}".format(out.value)
+        self.assertEqual(out.value, a.value)
+
+        print 'test_seven_node_tree_again: end'
