ca4bd9c4d3cf36ea613efb11481ddf6dd984da62
Christopher Roland <brianroland@Christophers-Mac-mini.local>
2016-10-19 14:51:23 -0400

    BR: finished problem_56

--- mongan_56/tests_mongan_56/test_unbalanced_bst.py
+++ mongan_56/tests_mongan_56/test_unbalanced_bst.py
@@ -1,4 +1,6 @@
 import unittest
+from mongan_54.lowest_common_ancestor import *
+from mongan_56.unbalanced_bst import *
 
 
 class TestUnbalancedBST(unittest.TestCase):
@@ -7,3 +9,38 @@ class TestUnbalancedBST(unittest.TestCase):
         print 'TestHelloWorld: beg'
         print 'TestHelloWorld: end'
 
+    def TestThreeNodeTree(self):
+        print 'TestThreeNodeTree: beg'
+
+        a = Node(80)
+        b = Node(40)
+        c = Node(120)
+        d = Node(20)
+        e = Node(60)
+        f = Node(100)
+
+        i = Node(50)
+        j = Node(70)
+
+        a.left = b
+        b.parent = a
+        a.right = c
+        c.parent = a
+
+        b.left = d
+        d.parent = b
+        b.right = e
+        e.parent = b
+
+        c.left = f
+        f.parent = c
+
+        e.left = i
+        i.parent = e
+
+        e.right = j
+        j.parent = e
+
+        right_rotate_bst(a)
+
+        print 'TestThreeNodeTree: end'
--- mongan_56/unbalanced_bst.py
+++ mongan_56/unbalanced_bst.py
@@ -1,6 +1,39 @@
+import mongan_54.lowest_common_ancestor
 
 
+def is_left_child(node):
+
+    return (node.parent is not None) and (node.parent.left is node)
 
 
-def BalanceLeftHeavyBST(root):
-    pass
+def is_right_child(node):
+
+    return (node.parent is not None) and (node.parent.right is node)
+
+
+def right_rotate_bst(root):
+
+    #these are the addresses of the objects we'll modify
+    old_root = root
+    old_left = root.left
+    old_left_right = root.left.right
+
+    #connect up new root
+    if is_left_child(old_root):
+        old_root.parent.left = old_left
+        old_left.parent = old_root.parent
+
+    elif is_right_child(old_root):
+        old_root.parent.right = old_left
+        old_left.parent = old_root.parent
+
+    else:
+        old_left.parent = None
+
+    #connect old_root as right child of old_left
+    old_left.right = old_root
+    old_root.parent = old_left
+
+    #connect old_left_right as left child of old_root
+    old_root.left = old_left_right
+    old_left_right.parent = old_root
